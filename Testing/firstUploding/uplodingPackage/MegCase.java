b87d95771c21d8a665117508de8c6be3adb5d31d
Meg <meg@192.168.1.40>
2019-02-01 14:17:03 -0500

    letest update

--- Testing/firstUploding/uplodingPackage/MegCase.java
+++ Testing/firstUploding/uplodingPackage/MegCase.java
@@ -5,6 +5,9 @@ public class MegCase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("I am Greate ");
+for (int i=0; i<10; i++) {
+	
+}
 	}
 
 }
