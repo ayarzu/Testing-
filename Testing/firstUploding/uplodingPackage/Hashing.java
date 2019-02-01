ef4553f16b71449402ef9159e5c6da19595f0f19
ayarzu <46764964+ayarzu@users.noreply.github.com>
2019-02-01 14:28:39 -0500

    Create Hashing.java

---
+++ Testing/firstUploding/uplodingPackage/Hashing.java
@@ -0,0 +1,29 @@
+public class Hashing {
+	
+	 public static void main(String[] args) {
+		    
+		    Set<Person> set = new HashSet<>(); 
+		    Person p1 = new Person(12,"John") ; 
+		    Person p2 = new Person(12,"John") ; 
+		     
+		    System.out.println(p1);
+
+		    set.add(  p1  ); 
+		    set.add(  p2 ) ; 
+		    
+		    System.out.println(set);
+
+		  }
+
+		}
+
+		class Person{
+		  
+		  int age; 
+		  String name;
+		  
+		  public Person(int age, String name) {
+		    super();
+		    this.age = age;
+		    this.name = name;
+		  }
